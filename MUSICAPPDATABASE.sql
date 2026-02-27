251fa51f8ef3fdfc8b455318bdb1794ce3fd664a
Samrin Adhikari <125154602+Samrinnnnn@users.noreply.github.com>
2026-02-27 12:39:21 +0545

    pg_indexes

--- MUSICAPPDATABASE.sql
+++ MUSICAPPDATABASE.sql
@@ -234,3 +234,9 @@ CREATE INDEX idx_tenants_song ON songs(tenant_id);
 CREATE INDEX idx_added_by ON songs(added_by,tenant_id);
 CREATE INDEX idx_tenants_user ON listener_profiles(tenant_id,user_name);
 CREATE INDEX idx_subtenant_user ON premium_subscription(tenant_id,user_name);
+
+--FIND index applied--
+SELECT tablename, indexname FROM pg_indexes 
+WHERE schemaname = 'public' 
+ORDER BY tablename;
+
